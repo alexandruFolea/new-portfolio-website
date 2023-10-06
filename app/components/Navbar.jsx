3e5c46709af2f25a9d3373c92b96dfeb46760ecf
Alex <alexandrufolea2@gmail.com>
2023-10-06 13:03:16 -0400

    added navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
+// DropdownMenu.js
+'use client';
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import Link from 'next/link';
-import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
-export default function Navbar() {
+export default function DropdownMenu() {
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+	const toggleDropdown = () => {
+		setIsDropdownOpen(!isDropdownOpen);
+	};
+
 	return (
-		<nav className='flex items-center justify-between mx-auto max-w-5xl p-6 bg-blue-200'>
-			<div className='logo'>alx.dev</div>
-			<ul className='flex gap-6'>
-				<li>
-					<Link href='/'>Home</Link>
-				</li>
-				<li>
-					<Link href='/projects'>Projects</Link>
-				</li>
-				<li>
-					<Link href='/contact'>Contact</Link>
-				</li>
-			</ul>
-		</nav>
+		<div className='sticky top-0 flex items-center justify-between w-full max-w-4xl py-4 m-auto'>
+			{/* Button to trigger the dropdown */}
+			<div className='relative z-50 px-4 py-2 text-3xl font-bold'>
+				<Link href='/'>alx.dev</Link>
+			</div>
+			<div>
+				<button
+					onClick={toggleDropdown}
+					className='relative z-10 flex items-center px-4 py-2 space-x-2 text-3xl'
+				>
+					{isDropdownOpen ? <FaTimes /> : <FaBars />}
+				</button>
+			</div>
+
+			{/* Dropdown container */}
+			<div
+				className={`absolute top-0 left-0 w-full h-96 overflow-hidden transition-all duration-900 ease-in-out transform ${
+					isDropdownOpen ? 'translate-y-1/8' : '-translate-y-full'
+				}`}
+			>
+				{/* Dropdown content goes here */}
+				<ul className='flex flex-col items-center justify-center h-full p-4 bg-white '>
+					<li>
+						<Link href='/projects' onClick={toggleDropdown}>
+							Projects
+						</Link>
+					</li>
+					<li>
+						<Link href='/contact' onClick={toggleDropdown}>
+							Contact
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
 	);
+	``;
 }
